b465caf7a5307e74de4b294a8360102bb81902d1
Damian Evans <damian@bluesky.eu.com>
2025-06-11 22:45:15 +0100

    hard-coded loop file

--- busker-amp.py
+++ busker-amp.py
@@ -121,7 +121,7 @@ def inputLoop():
                     loop_play.stop()
        
                 # Recreate the player with the new recorded content
-                loop_play = pyo64.SfPlayer(loop_file, loop=True, mul=loop_vol,).out()
+                loop_play = pyo64.SfPlayer('/home/admin/Documents/guitarpedal/pedalloop.wav', loop=True, mul=loop_vol,).out()
                 loop_play.play()  # Start playback of the loop                
             elif localLooperState == "STOPPED":
                 # Stop everything but keep recorded content
@@ -145,7 +145,7 @@ def inputLoop():
                 shutil.copy(silence, loop_file)
                 
                 # Recreate player with empty file
-                loop_play = pyo64.SfPlayer(loop_file, loop=True, mul=loop_vol)
+                loop_play = pyo64.SfPlayer('/home/admin/Documents/guitarpedal/pedalloop.wav', loop=True, mul=loop_vol)
                 loop_play.stop()
                 print("Loop erased")
 
